c5f4e1d812ef35a210bf897f9f47995ec3c9cd16
tanviredu <ornobtanvir.git@gmail.com>
2020-05-29 08:46:51 +0600

    added

--- ConsoleApp1/Data.Data/DatContext.cs
+++ ConsoleApp1/Data.Data/DatContext.cs
@@ -1,8 +1,19 @@
 ﻿using System;
+using Data.Domain;
+using Microsoft.EntityFrameworkCore;
 
 namespace Data.Data
 {
-    public class Class1
+    public class DataContext : DbContext
     {
+        // adding dbsets
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=A:\\LearnEntityFramework\\ConsoleApp1\\database.db");
+            
+        }
     }
 }
--- ConsoleApp1/Data.Domain/Author.cs
+++ ConsoleApp1/Data.Domain/Author.cs
@@ -1,8 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Data.Domain
 {
-    public class Class1
+    public class Author
     {
+        public int AuthorId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public List<Book> Books { get; set; }
     }
 }
